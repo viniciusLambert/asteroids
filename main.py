e59ae21ca98a70786aa270c40e9bc227572207da
vinicius.lambert <vinicius.lamber@proton.me>
2026-01-19 15:15:59 -0300

    add time tracking to main

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f'Screen height: {SCREEN_HEIGHT}')
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0    
+    
     while True:
         log_state()
         for event in pygame.event.get():
@@ -20,5 +23,7 @@ def main():
             pass
         screen.fill('black')
         pygame.display.flip()
+        timePassed = clock.tick(60)
+        dt = timePassed/1000
 if __name__ == "__main__":
     main()
